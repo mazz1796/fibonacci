418c835006c7ae36210cf9ace84f5401db2fc351
Koji Hiramatsu <mazz1796@gmail.com>
2016-09-08 11:39:54 +0800

    fibonacci

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,5 +1,16 @@
 def fibonacci_iterative(n)
+	array = [0,1]
+  until (array.length - 1) >= n
+    array << (array[-1] + array[-2])
+  end
+  array[n]
 end
 
 def fibonacci_recursive(n)
+	return 0 if n == 0
+  return 1 if n == 1
+  fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
 end
+
+p fibonacci_recursive(0) 
+p fibonacci_iterative(20)
